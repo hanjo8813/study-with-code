0199076eca9a30a7750c6abc9e6ad7b00133947c
한재원 <hanjo8813@gmail.com>
2021-07-20 02:44:18 +0900

    chore : init spring boot project

---
+++ Spring_Boot/src/test/java/com/boot_test/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot_test.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
