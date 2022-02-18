0aaeb7bc4349fe49e37adc9569f3692e6a48edc7
hanjo8813 <hanjo8813@gmail.com>
2022-02-19 00:25:12 +0900

    init logback

---
+++ spring-logback/src/main/java/com/example/springlogback/SpringLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogbackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringLogbackApplication.class, args);
+  }
+
+}
--- spring-logback/src/test/java/com/example/springlogback/SpringLogbackApplicationTests.java
+++ spring-logback/src/test/java/com/example/springlogback/SpringLogbackApplicationTests.java
@@ -1,13 +1,13 @@
-package com.boot_test.springboot;
+package com.example.springlogback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpringLogbackApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
