2c915cf5e8e722748d16d9d953218063d096987b
hanjo8813 <hanjo8813@gmail.com>
2022-06-11 15:20:32 +0900

    init kotlin practice

---
+++ kotlin-study/build.gradle.kts
@@ -0,0 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "me.nb1"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
