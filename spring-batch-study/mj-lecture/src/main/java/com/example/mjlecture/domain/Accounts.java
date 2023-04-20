b4c10a90d3c1a9f8b87a234d593c7a7da6b97622
hanjo8813 <hanjo8813@gmail.com>
2023-04-20 23:53:15 +0900

    batch

---
+++ spring-batch-study/mj-lecture/src/main/java/com/example/mjlecture/domain/Accounts.java
@@ -0,0 +1,33 @@
+package com.example.mjlecture.domain;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
+public class Accounts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String orderItem;
+    private Integer price;
+    private Date orderDate;
+    private Date accountDate;
+
+    public Accounts(Orders orders) {
+        this.id = orders.getId();
+        this.orderItem = orders.getOrderItem();
+        this.price = orders.getPrice();
+        this.orderDate = orders.getOrderDate();
+        this.accountDate = new Date();
+    }
+}
