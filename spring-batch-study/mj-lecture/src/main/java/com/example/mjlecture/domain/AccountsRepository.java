df688ca98deb4acbd511f9782bc5448bbe7994f9
hanjo8813 <hanjo8813@gmail.com>
2023-05-11 00:23:22 +0900

    batch 삭제

--- spring-batch-study/mj-lecture/src/main/java/com/example/mjlecture/domain/AccountsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.mjlecture.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountsRepository  extends JpaRepository<Accounts, Integer> {
-}
